24247486ea0b7201397345990f7d8bfa90262892
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2025-03-08 19:05:16 +0530

    new

--- club-master-frontend/src/App.tsx
+++ club-master-frontend/src/App.tsx
@@ -17,7 +17,7 @@ import Rotractlogin  from './pages/Rotractlogin';
 // import Adminsidebar  from './components/Adminsidebar';
 import Rotract  from './Rotractclub/Rotract';
 import Leologin  from './pages/Leologin';
-import Clubadmin  from './components/Clubadmin';
+// import Clubadmin  from './components/Clubadmin';
 import Isacalogin  from './pages/Isacalogin';
 import Rotractcontact  from './Rotractclub/Rotractcontact';
 import Rotractabout  from './Rotractclub/Rotractabout';
@@ -27,6 +27,7 @@ import Userprofile  from './pages/Userprofile';
 import { UserProvider } from "./common/UserContext";
 import ViewMyClubs from "./pages/ViewMyClubs";
 import MainAdminDashboard from "./admin/MainAdminDashboard";
+// import ViewMyEvents from "./pages/ViewMyEvents";
 
 
 
@@ -63,6 +64,7 @@ function App() {
       {/* <Route path="newclub" element={<Newclub/>}/>  */}
       <Route path="myclub" element={<ViewMyClubs/>}/>
       <Route path="mainAdmin" element={<MainAdminDashboard/>}/>
+      {/* <Route path="myOnGoingEvents" element={<ViewMyOngoingEvents/>}/> */}
      
      </Routes>
     </BrowserRouter>
--- club-master-frontend/src/components/Sidebar.jsx
+++ club-master-frontend/src/components/Sidebar.jsx
@@ -1,35 +1,70 @@
-import React from "react";
-import { FaUsers, FaCalendarAlt, FaNewspaper, FaCalendar, FaTachometerAlt } from "react-icons/fa";
-import { Link } from "react-router-dom"; // Import Link
+import React, { useState } from "react";
+import { FaUsers, FaCalendarAlt, FaNewspaper, FaCalendar, FaTachometerAlt, FaChevronDown, FaChevronUp } from "react-icons/fa";
+import { Link } from "react-router-dom";
 import './Sidebar.css';
 
 const Sidebar = () => {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(selectedCategory === category ? null : category);
+  };
+
   return (
     <div className="sidebar">
-      <ul>
-      <li>
+      <ul className="sidebar-menu">
+        <li className="sidebar-item">
           <Link to="/clubHome" className="sidebar-link">
             <FaTachometerAlt className="icon" /> Dashboard
           </Link>
         </li>
-        
-        <li>
+
+        <li className="sidebar-item">
           <Link to="/myclub" className="sidebar-link">
             <FaUsers className="icon" /> My Clubs
           </Link>
         </li>
-        
-        <li>
-          <Link to="/events" className="sidebar-link">
-            <FaCalendarAlt className="icon" /> Events
-          </Link>
+
+        <li className={`sidebar-item ${selectedCategory === "Events" ? "active" : ""}`}>
+          <div 
+            className="sidebar-link dropdown-toggle"
+            onClick={() => handleCategoryClick("Events")}
+          >
+            <div className="link-content">
+              <FaCalendarAlt className="icon" /> 
+              <span>Events</span>
+            </div>
+            {selectedCategory === "Events" ? <FaChevronUp className="dropdown-icon" /> : <FaChevronDown className="dropdown-icon" />}
+          </div>
+          
+          {selectedCategory === "Events" && (
+            <ul className="sidebar-submenu">
+              <li className="submenu-item">
+                <Link to="/myOnGoingEvents" className="submenu-link">
+                  Ongoing Events
+                </Link>
+              </li>
+              <li className="submenu-item">
+                <Link to="/upcoming-events" className="submenu-link">
+                  Upcoming Events
+                </Link>
+              </li>
+              <li className="submenu-item">
+                <Link to="/past-events" className="submenu-link">
+                  Past Events
+                </Link>
+              </li>
+            </ul>
+          )}
         </li>
-        <li>
+
+        <li className="sidebar-item">
           <Link to="/news" className="sidebar-link">
             <FaNewspaper className="icon" /> News
           </Link>
         </li>
-        <li>
+
+        <li className="sidebar-item">
           <Link to="/calendar" className="sidebar-link">
             <FaCalendar className="icon" /> Calendar
           </Link>
