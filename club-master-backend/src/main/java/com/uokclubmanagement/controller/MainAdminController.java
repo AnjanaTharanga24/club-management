88132d02a7b39a9bbdf249d19d8508af30bcb686
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2025-03-08 11:57:03 +0530

    new

--- club-master-backend/src/main/java/com/uokclubmanagement/controller/MainAdminController.java
+++ club-master-backend/src/main/java/com/uokclubmanagement/controller/MainAdminController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/mainAdmin")
+@CrossOrigin(origins = "http://localhost:5173")
 public class MainAdminController {
 
     @Autowired
